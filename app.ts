3eec6f136fbdd2ecfcec9db83363dd6a9cc0f695
Alexey <alexey6486@gmail.com>
2020-10-06 13:07:49 +0300

    api_GET-details

--- app.ts
+++ app.ts
@@ -1,14 +1,28 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // middleware for POST request.body
+
+const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Allow-Credentials": "false"
+};
 
 let PAINTINGS_DB = JSON.parse(fs.readFileSync(`${__dirname}/data/paintings.json`));
 
+// get all paintings
 app.get('/api/v1/paintings', (request, response) => {
     response
+        .set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods" : "GET",
+        })
         .status(200)
         .json({
             status: 'success',
@@ -19,6 +33,32 @@ app.get('/api/v1/paintings', (request, response) => {
         })
 });
 
+// get painting's details
+app.get(`/api/v1/paintings/:id`, (request, response) => {
+    const details = PAINTINGS_DB.items.find(painting => painting.id === request.params.id);
+
+    if (!details) {
+        return response.status(404).json({
+            status: 'Error',
+            message: 'ID hasn\'t been found'
+        })
+    }
+
+    response
+        .set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods" : "GET",
+        })
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                painting: details
+            },
+        })
+})
+
+// add a new painting
 app.post('/api/v1/paintings', (request, response) => {
 
     const newId = new Date().getTime();
@@ -31,6 +71,10 @@ app.post('/api/v1/paintings', (request, response) => {
         console.log(error ? error : 'a painting was added, no errors occurred');
     })
     response
+        .set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods" : "POST",
+        })
         .status(201)
         .json({
             status: 'success',
@@ -39,6 +83,7 @@ app.post('/api/v1/paintings', (request, response) => {
             }
         });
 });
+
 
 ////////////////////////////////////////////
 const port = 3001;
