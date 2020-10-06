66613bf256dfabedff8b1582d65086365888e1ec
Alexey <alexey6486@gmail.com>
2020-10-06 13:28:28 +0300

    api_routes_refactored

--- app.ts
+++ app.ts
@@ -16,8 +16,7 @@ const headers = {
 
 let PAINTINGS_DB = JSON.parse(fs.readFileSync(`${__dirname}/data/paintings.json`));
 
-// get all paintings
-app.get('/api/v1/paintings', (request, response) => {
+const getAllPaintings = (request, response) => {
     response
         .set({
             "Access-Control-Allow-Origin": "*",
@@ -31,10 +30,8 @@ app.get('/api/v1/paintings', (request, response) => {
                 paintings: PAINTINGS_DB
             },
         })
-});
-
-// get painting's details
-app.get(`/api/v1/paintings/:id`, (request, response) => {
+};
+const getPaintingDetails = (request, response) => {
     const details = PAINTINGS_DB.items.find(painting => painting.id === request.params.id);
 
     if (!details) {
@@ -56,10 +53,8 @@ app.get(`/api/v1/paintings/:id`, (request, response) => {
                 painting: details
             },
         })
-})
-
-// add a new painting
-app.post('/api/v1/paintings', (request, response) => {
+};
+const addNewPainting = (request, response) => {
 
     const newId = new Date().getTime();
     const newPainting = {
@@ -82,11 +77,63 @@ app.post('/api/v1/paintings', (request, response) => {
                 painting: newPainting
             }
         });
-});
+};
+const updatePainting = (request, response) => {
+    const details = PAINTINGS_DB.items.find(painting => painting.id === request.params.id);
 
+    if (!details) {
+        return response.status(404).json({
+            status: 'Error',
+            message: 'ID hasn\'t been found'
+        })
+    }
+
+    response
+        .set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods" : "PATCH",
+        })
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                painting: 'data updated'
+            },
+        })
+};
+const deletePainting = (request, response) => {
+    const details = PAINTINGS_DB.items.find(painting => painting.id === request.params.id);
+
+    if (!details) {
+        return response.status(404).json({
+            status: 'Error',
+            message: 'ID hasn\'t been found'
+        })
+    }
+
+    response
+        .set({
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods" : "PATCH",
+        })
+        .status(204)
+        .json({
+            status: 'success',
+            data: null,
+        })
+};
+
+app.route('/api/v1/paintings')
+    .get(getAllPaintings)
+    .post(addNewPainting);
+
+app.route('/api/v1/paintings/:id')
+    .get(getPaintingDetails)
+    .patch(updatePainting)
+    .delete(deletePainting);
 
 ////////////////////////////////////////////
 const port = 3001;
 app.listen(port, () => {
     console.log(`Listening on ${port}...`)
-})
+});
