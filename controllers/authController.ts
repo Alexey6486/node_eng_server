0543ea095aeb18b99ab3fc5bf3fffff4439d8f3a
Alexey <alexey6486@gmail.com>
2020-10-13 09:49:38 +0300

    jwt

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,13 +1,24 @@
 import {NextFunction, Request, Response} from "express";
 const catchAsyncAuth = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 const UserModelForAuth = require('../models/usersModel.ts');
 
 exports.signup = catchAsyncAuth(async (request: Request, response: Response, next: NextFunction) => {
-    const newUser = await UserModelForAuth.create(request.body);
+
+    // we must specify fields to not allow anybody to create an account where "isAdmin" field is set to true
+    // ...create(request.body) - is wrong
+    const newUser = await UserModelForAuth.create({
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password,
+    });
+
+    const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
 
     response.status(201).json({
         status: 'success',
+        token: token,
         data: {
             user: newUser
         }
